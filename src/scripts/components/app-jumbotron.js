792cdb79dec4ec1751901734be8acf5dcb5d0af8
KrsNovita2717 <novitakristiana2717@gmail.com>
2023-11-02 20:23:50 +0700

    image responsive

--- src/scripts/components/app-jumbotron.js
+++ src/scripts/components/app-jumbotron.js
@@ -6,9 +6,10 @@ class AppJumbotron extends HTMLElement {
   render() {
     this.innerHTML = `
       <div class="hero-image">
-        <figure>
-          <img src="images/hero-image.jpg" alt="Tampilan meja yang ada di restoran">
-        </figure>
+        <picture>
+          <source media="(max-width: 600px)" srcset="./images/hero-image-small.jpg">
+          <img src='./images/hero-image-large.jpg' alt="Tampilan meja yang ada di restoran">
+        </picture>
       </div>
       <div class="inner">
         <h1 class="title">Flavoriesta</h1>
